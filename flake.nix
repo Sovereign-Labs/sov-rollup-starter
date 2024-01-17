dbcfba55a35ee3be6080d5416aabe8995e783745
Soares Chen <soares.chen@maybevoid.com>
2024-01-17 21:17:52 +0100

    Rollup Nix build is working

--- flake.nix
+++ flake.nix
@@ -153,16 +153,17 @@
             cargoLock = {
                 lockFile = ./Cargo.lock;
                 outputHashes = {
-                  "celestia-proto-0.1.0" = "sha256-iUgrctxdJUyhfrEQ0zoVj5AKIqgj/jQVNli5/K2nxK0=";
-                  "jmt-0.9.0" = "sha256-pq1v6FXS//6Dh+fdysQIVp+RVLHdXrW5aDx3263O1rs=";
-                  "nmt-rs-0.1.0" = "sha256-jcHbqyIKk8ZDDjSz+ot5YDxROOnrpM4TRmNFVfNniwU=";
-                  "sov-accounts-0.3.0" = "sha256-Bmzo0xe1GdSKEIAyYx0PHhauNdVBMawOSSIflhdfi6U=";
-                  "tendermint-0.32.0" = "sha256-FtY7a+hBvQryATrs3mykCWFRe8ABTT6cuf5oh9IBElQ=";
-                  "bonsai-sdk-0.5.1" = "sha256-Av3rpNhDny8FroOcn8eyvZcR8hFSNukA7n9impm1HHU=";
+                    "celestia-proto-0.1.0" = "sha256-iUgrctxdJUyhfrEQ0zoVj5AKIqgj/jQVNli5/K2nxK0=";
+                    "jmt-0.9.0" = "sha256-pq1v6FXS//6Dh+fdysQIVp+RVLHdXrW5aDx3263O1rs=";
+                    "nmt-rs-0.1.0" = "sha256-jcHbqyIKk8ZDDjSz+ot5YDxROOnrpM4TRmNFVfNniwU=";
+                    "sov-accounts-0.3.0" = "sha256-Bmzo0xe1GdSKEIAyYx0PHhauNdVBMawOSSIflhdfi6U=";
+                    "tendermint-0.32.0" = "sha256-FtY7a+hBvQryATrs3mykCWFRe8ABTT6cuf5oh9IBElQ=";
+                    "bonsai-sdk-0.5.1" = "sha256-Av3rpNhDny8FroOcn8eyvZcR8hFSNukA7n9impm1HHU=";
                 };
             };
 
             buildType = "debug";
+            doCheck = false;
             buildNoDefaultFeatures = true;
             buildFeatures = [ "celestia_da" ];
 
@@ -175,6 +176,7 @@
 
             # ROLLUP_ELF_PATH = "${rollup-guest-celestia}/rollup";
             SKIP_GUEST_BUILD = "1";
+            CONSTANTS_MANIFEST = ./.;
 
             nativeBuildInputs = [
                 nixpkgs.pkg-config
